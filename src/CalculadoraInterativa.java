c7ee49c7831d562c8806b2fb2db82dc8a77a5f16
Felipe Honorio <felipehonorio734@gmail.com>
2026-03-26 11:09:17 -0300

    Update CalculadoraInterativa.java

--- src/CalculadoraInterativa.java
+++ src/CalculadoraInterativa.java
@@ -1,1 +1,39 @@
+import java.util.Scanner;
 
+public class CalculadoraInterativa {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int total = 0;
+        int opcao;
+
+        System.out.println("Bem-vindo à Calculadora Interativa");
+
+        do {
+            System.out.println("Total atual: " + total);
+            System.out.println("1 - Somar um número");
+            System.out.println("2 - Subtrair um número");
+            System.out.println("0 - Sair e ver resultado final");
+            System.out.println("Escolha uma opção: ");
+            opcao = sc.nextInt();
+
+            if (opcao == 1) {
+                System.out.println("Digite o valor para somar: ");
+                int valor = sc.nextInt();
+                total += valor;
+            } else if (opcao == 2) {
+                System.out.println("Digite o valor para subtrair: ");
+                int valor = sc.nextInt();
+                total -= valor;
+            } else if (opcao != 0) {
+                System.out.println("Opção inválida! Tente novamente.");
+            }
+        } while (opcao != 0);
+
+        System.out.println("Resultado final: " + total);
+        System.out.println("Programa encerrado!");
+
+        sc.close();
+    }
+}
